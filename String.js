dc80fc9655f02b19a39cee954f2f60bf43be8da2
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-02-05 19:04:58 +0530

    string

---
+++ String.js
@@ -0,0 +1,9 @@
+var str1 = "hello";
+var str2 = 'hello';
+var str3String = "hello";
+var age = 20;
+var info = "My age is ".concat(age);
+console.log(info);
+var item = true;
+var data = "" + item;
+console.log(data);
