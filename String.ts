e59b506e93c4d39cf46cb14727975c91226d275e
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-02-05 19:04:45 +0530

    string

---
+++ String.ts
@@ -0,0 +1,13 @@
+var str1:String = "hello";
+var str2:String = 'hello';
+var str3String = `hello`;
+
+var age:number = 20;
+
+var info:String = `My age is ${age}`;
+console.log(info);
+
+var item:boolean= true;
+
+var data:string ="" +item
+console.log(data);
